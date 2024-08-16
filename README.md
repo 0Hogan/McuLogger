8235ab3a2555d3f5921285e6d906d8913f7375c2
Michael Hogan <mjhogan38@gmail.com>
2024-08-16 09:39:35 -0500

    Initial commit. First out logging is supported. Not yet extensively tested.

---
+++ README.md
@@ -0,0 +1,3 @@
+# MCU Logger
+
+A simple project to create a flexible logger intended for use on microcontrollers.
---
+++ examples/UartSerialLogging/UartSerialLogging.ino
@@ -0,0 +1,21 @@
+#include <Arduino.h>
+#include <Log.h>
+
+const uint16_t TIME_BETWEEN_LOGS_MS = 3000;
+
+void setup()
+{
+    Serial1.begin(115200);
+    Logger::addOutputStream(&Serial1);
+}
+
+uint64_t timeOfLastLog_ms = 0;
+void loop()
+{
+    if (millis() >= timeOfLastLog_ms + TIME_BETWEEN_LOGS_MS)
+    {
+        LOG_INFO("");
+        timeOfLastLog_ms = millis();
+    }
+    delay(100);
+}
